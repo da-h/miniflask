7e148d0168b842febdad458ee450275b17bc6ad9
David Hartmann <dhartmann@uni-mainz.de>
2020-02-26 15:03:37 +0100

    better colors for modules in 'event'

--- src/miniflask/modules/events/__init__.py
+++ src/miniflask/modules/events/__init__.py
@@ -1,5 +1,12 @@
 from miniflask.dummy import miniflask_dummy
 from colored import fg, bg, attr
+from miniflask.miniflask import highlight_module
+
+def color_module(moduleid):
+    moduleid = moduleid.split(".")
+    moduleid[0] = attr('dim')+moduleid[0]
+    moduleid[-1] = attr('reset')+highlight_module(moduleid[-1])
+    return ".".join(moduleid)
 
 def register(mf):
     events = {}
@@ -26,5 +33,5 @@ def register(mf):
     print(fg('yellow')+attr('underlined')+"Available events"+attr('reset'))
     for e in sorted(events.keys()):
         unique_flag = "!" if e[1] else ">"
-        print(fg('yellow')+unique_flag+' '+e[0]+attr('reset')+" used in "+(attr('reset')+", "+fg('green')+attr('bold')).join(events[e])+attr('reset'))
+        print(fg('yellow')+unique_flag+' '+e[0]+attr('reset')+" used in "+", ".join([color_module(ev) for ev in events[e]])+attr('reset'))
     print()
