f6659c1c50fbf4ccf45ee6c9456b3abd817915f9
David Hartmann <dhartmann@uni-mainz.de>
2020-03-02 14:45:17 +0100

    'settings'-module: removed uneeded parsing to dict

--- src/miniflask/modules/settings/__init__.py
+++ src/miniflask/modules/settings/__init__.py
@@ -2,23 +2,7 @@ import sys
 from itertools import zip_longest
 from miniflask.miniflask import highlight_module, highlight_val, highlight_name, highlight_val_overwrite
 
-def insert_key(S,k,val):
-
-    # create dict if not existent
-    if k[0] not in S:
-        S_new = {}
-        S[k[0]] = S_new
-
-    # insert keys recursively
-    if len(k) > 1:
-        if isinstance(S[k[0]],dict):
-            S[k[0]] = {'_': S[k[0]]}
-        insert_key(S[k[0]], k[1:], val)
-    else:
-        S[k[0]] = val
-
 def listsettings(state, event):
-    S = {}
     last_k = []
     if len(state.all) == 0:
         print("No Settings available.")
@@ -36,7 +20,6 @@ def listsettings(state, event):
         korig = k
         overwritten = v != state.default[k]
         k = k.split(".")
-        insert_key(S,k,v)
         if len(k) > 1:
             k_hidden = [" "*len(ki) if ki==ki2 else ki for ki,ki2 in zip_longest(k,last_k) if ki is not None]
             last_k = k
