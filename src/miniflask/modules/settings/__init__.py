3bc52a01b4d70d30546d599723c4a6b1a23b8c59
David Hartmann <dhartmann@uni-mainz.de>
2020-03-03 12:43:22 +0100

    added (simplified) settings_html to settings module

--- src/miniflask/modules/settings/__init__.py
+++ src/miniflask/modules/settings/__init__.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from itertools import zip_longest
 from miniflask.miniflask import highlight_module, highlight_val, highlight_name, highlight_val_overwrite
 
@@ -8,9 +9,9 @@ def listsettings(state, event):
         print("No Settings available.")
         return
     maxklen = max(len(k) for k in state.all.keys())
-    print("Folder│"+highlight_name("module")+"│"+highlight_module("variable")+(" "*(maxklen-22))+" = "+highlight_val("value"))
-    print("—"*(maxklen+8))
-    for k, v in state.all.items():
+    text = "Folder│"+highlight_name("module")+"│"+highlight_module("variable")+(" "*(maxklen-22))+" = "+highlight_val("value")+os.linesep
+    text += "—"*(maxklen+8)+os.linesep
+    for k, v in sorted(state.all.items()):
 
         # ignore state variables that are not registered for argument parsing
         if k not in state.default:
@@ -29,8 +30,20 @@ def listsettings(state, event):
             k_hidden = k
             k_hidden[-1] = highlight_module(k_hidden[-1])
 
-        append = "" if not overwritten else " -> "+highlight_val_overwrite(str(v))
-        print("│".join(k_hidden)+(" "*(maxklen-klen))+" = "+highlight_val(str(state.default[korig]))+append)
+        append = "" if not overwritten else " ⟶   "+highlight_val_overwrite(str(v))
+        text += "│".join(k_hidden)+(" "*(maxklen-klen))+" = "+highlight_val(str(state.default[korig]))+append+os.linesep
+
+    return text
+
+
+def init(state,event):
+    print(listsettings(state,event))
+
+def settings_html(state,event):
+    html = "<code>"+listsettings(state,event)+"</code>"
+    return html
+
 
 def register(mf):
-    mf.register_event("init", listsettings)
+    mf.register_event("init", init)
+    mf.register_event("settings_html", settings_html)
