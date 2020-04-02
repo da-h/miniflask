808351066e1b1667e22ab8068a80454e3ca403c2
Sebastian Brodehl <brodehl@uni-mainz.de>
2020-04-02 17:47:41 +0200

    Add project meta data.

--- src/miniflask/__init__.py
+++ src/miniflask/__init__.py
@@ -5,6 +5,18 @@ from .state import like
 
 # meta
 __version__ = "1.9"
+__title__         = "miniflask"
+__description__   = "Small research-oriented hook-based plugin engine."
+__url__           = "https://github.com/da-h/miniflask"
+__uri__           = __url__
+__doc__           = __description__ + " <" + __uri__ + ">"
+__documentation__ = 'https://da-h.github.io/miniflask'
+__source__        = __url__
+__tracker__       = __url__ + '/issues'
+__author__ = "David Hartmann"
+__license__ = "MIT License"
+__copyright__ = "Copyright (c) 2020 David Hartmann"
+
 
 def init(**kwargs):
     return miniflask(**kwargs)
