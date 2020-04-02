3597176f5e4b2c2256fb4426ef9c4f99027b0057
Sebastian Brodehl <brodehl@uni-mainz.de>
2020-04-02 17:47:41 +0200

    Added setup.py

--- src/miniflask/__init__.py
+++ src/miniflask/__init__.py
@@ -4,7 +4,19 @@ from .modules import *
 from .state import like
 
 # meta
-__version__ = "1.9"
+__version__       = "1.9"
+__title__         = "miniflask"
+__description__   = "Small research-oriented hook-based plugin engine."
+__url__           = "https://github.com/da-h/miniflask"
+__uri__           = __url__
+__doc__           = __description__ + " <" + __uri__ + ">"
+__documentation__ = 'https://da-h.github.io/miniflask'
+__source__        = __url__
+__tracker__       = __url__ + '/issues'
+__author__        = "David Hartmann"
+__license__       = "MIT License"
+__copyright__     = "Copyright (c) 2020 " + __author__
+
 
 def init(**kwargs):
     return miniflask(**kwargs)
