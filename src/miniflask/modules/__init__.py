07109d4772f84599d4a884d53a5d8d4bbb7a4eb1
David Hartmann <dhartmann@uni-mainz.de>
2020-07-13 18:18:01 +0200

    changed module_ids of predefined modules miniflask.modules.* -> miniflask.*

--- src/miniflask/modules/__init__.py
+++ src/miniflask/modules/__init__.py
@@ -1,9 +1,10 @@
 
 def registerPredefined(modules_avail):
     for m in ["modules", "events", "info", "settings"]:
-        module_name_id = 'miniflask.modules.'+m
+        module_name_id = 'miniflask.'+m
+        importpath = 'miniflask.modules.'+m
         modules_avail[module_name_id] = {
             'id': module_name_id,
-            'importpath': module_name_id,
+            'importpath': importpath,
             'lowpriority': False
         }
