928b51e22e042fb45b6e76a7e713812bd6848554
David Hartmann <dhartmann@uni-mainz.de>
2020-10-06 14:00:18 +0200

    added testcase for delitem-operator

--- tests/temporary/modules/models/dosomething/__init__.py
+++ tests/temporary/modules/models/dosomething/__init__.py
@@ -1,8 +1,14 @@
+from miniflask.exceptions import StateKeyError
 
 def dosomething(state, event):
-    print("in event", state["variable"])
+    print("in event: variable =", state["variable"])
+    if "new_variable" in state:
+        print("in event: new_variable =", state["new_variable"])
 
 def main(state, event):
+
+    state["new_variable"] = 42
+    del state["new_variable"]
 
     print("before event", state["variable"])
     with state.temporary({
@@ -10,6 +16,22 @@ def main(state, event):
     }):
         event.dosomething()
     print("after event", state["variable"])
+
+    try:
+        state["new_variable"]
+        print("variable 'new_variable' should not exist")
+    except StateKeyError:
+        pass
+    with state.temporary({
+        "new_variable": 12345
+    }):
+        event.dosomething()
+    try:
+        state["new_variable"]
+        print("variable 'new_variable' should not exist")
+    except StateKeyError:
+        pass
+
 
 def register(mf):
     mf.register_defaults({
