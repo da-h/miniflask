348939d7d2a77cfbd5df3b24c8d1efec8303b1e4
Sebastian Brodehl <brodehl@uni-mainz.de>
2020-11-19 10:50:53 +0100

    Add spaces around operators, fix newlines.

--- tests/outervar/mf_event.py
+++ tests/outervar/mf_event.py
@@ -1,7 +1,7 @@
 #!/bin/python
 import sys
 import os
-sys.path.insert(0, os.path.join("..","..","src"))
+sys.path.insert(0, os.path.join("..", "..", "src"))
 
 import miniflask
 mf = miniflask.init(
@@ -12,4 +12,3 @@ mf.load("module1")
 
 varA = 42
 mf.event.main()
-
--- tests/outervar/modules/module1/__init__.py
+++ tests/outervar/modules/module1/__init__.py
@@ -1,7 +1,9 @@
 from miniflask import outervar
 
+
 def main(state, event, varA=outervar):
-    print("outervar:",varA)
+    print("outervar:", varA)
+
 
 def register(mf):
     mf.register_event('main', main, unique=True)
