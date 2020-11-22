66420ae2187dcebf0744f23fd8070617244e4318
Sebastian Brodehl <brodehl@uni-mainz.de>
2020-11-22 20:02:25 +0100

    Remove unnecessary `elif` after `return`.

--- src/miniflask/util.py
+++ src/miniflask/util.py
@@ -62,10 +62,9 @@ def str2bool(v):
         return v
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+    if v.lower() in ('no', 'false', 'f', 'n', '0'):
         return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
+    raise argparse.ArgumentTypeError('Boolean value expected.')
 
 
 def get_varid_from_fuzzy(varid, varid_list):
