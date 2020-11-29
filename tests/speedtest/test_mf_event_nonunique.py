a9e053cc5d4a6f31b55b90d81cc3be4043e0d7c8
Sebastian Brodehl <brodehl@uni-mainz.de>
2020-11-29 16:28:46 +0100

    Write mf event nonunique speed test.

--- tests/speedtest/test_mf_event_nonunique.py
+++ tests/speedtest/test_mf_event_nonunique.py
@@ -1,15 +1,16 @@
-#!/bin/python
-import sys
-import os
-sys.path.insert(0, os.path.join("..", "..", "src"))
+from pathlib import Path
 
 import miniflask  # noqa: E402
-mf = miniflask.init(
-    module_dirs="./modules",
-)
-event = mf.event
-mf.load("modulenonunique")
 
-a = 0
-for i in range(10000000):
-    a += event.func(42)[0]
+mf = miniflask.init(
+    module_dirs=str(Path(__file__).parent / "modules"),
+)
+
+
+def test_mf_event_nonunique():
+    event = mf.event
+    mf.load("modulenonunique")
+
+    a = 0
+    for i in range(10000000):
+        a += event.func(42)[0]
