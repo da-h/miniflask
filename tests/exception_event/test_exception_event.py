7f12a29baf976bf41da58d4a788f8546de49d244
Sebastian Brodehl <brodehl@uni-mainz.de>
2020-11-29 16:18:05 +0100

    Write exception event test.

---
+++ tests/exception_event/test_exception_event.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+import miniflask  # noqa: E402
+
+mf = miniflask.init(
+    module_dirs=str(Path(__file__).parent / "modules"),
+)
+
+
+def test_exception_event():
+    mf.load("module1")
+    mf.register_event("main", lambda: print("Main."))
+    mf.run()
