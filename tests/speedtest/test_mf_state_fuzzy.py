1496fe7742a05a75b8078bda185ab340a0edf3bc
Sebastian Brodehl <brodehl@uni-mainz.de>
2020-11-29 16:30:37 +0100

    Write mf state fuzzy speed test.

--- tests/speedtest/test_mf_state_fuzzy.py
+++ tests/speedtest/test_mf_state_fuzzy.py
@@ -1,15 +1,16 @@
-#!/bin/python
-import sys
-import os
-sys.path.insert(0, os.path.join("..", "..", "src"))
+from pathlib import Path
 
 import miniflask  # noqa: E402
-mf = miniflask.init(
-    module_dirs="./modules",
-)
-mf.load("moduleunique")
-event = mf.event
 
-a = 0
-for i in range(10000000):
-    a += event.get_state_var(".moduleunique.a")
+mf = miniflask.init(
+    module_dirs=str(Path(__file__).parent / "modules"),
+)
+
+
+def test_mf_state_fuzzy():
+    mf.load("moduleunique")
+    event = mf.event
+
+    a = 0
+    for i in range(10000000):
+        a += event.get_state_var(".moduleunique.a")
