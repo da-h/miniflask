787366bb5166f549d751b23efd3d10adb2efe797
David Hartmann <dhartmann@uni-mainz.de>
2020-12-29 13:40:28 +0100

    tests: fixed relative import test

--- tests/test_import/test_relative_import.py
+++ tests/test_import/test_relative_import.py
@@ -17,8 +17,9 @@ def test_passive_submodule_import():
 def test_active_submodule_import():
     mf = init_mf()
     mf.run(argv=[], modules=["submodule"])
+    assert "modules.parentdir.module3" in mf.modules_loaded
     assert "modules.parentdir.module3.submodule" in mf.modules_loaded
-    assert len(mf.modules_loaded) == 1
+    assert len(mf.modules_loaded) == 2
 
 
 def test_relative_import():
