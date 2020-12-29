c65cfb82ce6ad6dfff352e6fc135f8c01fd21648
David Hartmann <dhartmann@uni-mainz.de>
2020-12-29 12:41:03 +0100

    tests: minor pylint fix

--- tests/used_case1/modules/datasets/__init__.py
+++ tests/used_case1/modules/datasets/__init__.py
@@ -1,8 +1,3 @@
 def register(mf):
+    del mf
     print("data")
-    pass
-    # mf.load(["cifar10","main_loop"])
-    # defaults = {
-    #     "num_runs": 2
-    # }
-    # mf.register_defaults(defaults)
