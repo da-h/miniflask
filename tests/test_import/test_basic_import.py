61a95a28543d8545628bb8909b8ff597294e6fb2
David Hartmann <dhartmann@uni-mainz.de>
2020-12-29 15:59:32 +0100

    tests: fixed missing ignore of sys.argv to all tests

--- tests/test_import/test_basic_import.py
+++ tests/test_import/test_basic_import.py
@@ -9,28 +9,28 @@ def init_mf():
 
 def test_shortid():
     mf = init_mf()
-    mf.run(modules=["module1"])
+    mf.run(argv=[], modules=["module1"])
     assert "modules.parentdir.module1" in mf.modules_loaded
     assert len(mf.modules_loaded) == 1
 
 
 def test_partial_id():
     mf = init_mf()
-    mf.run(modules=["modules.module1"])
+    mf.run(argv=[], modules=["modules.module1"])
     assert "modules.parentdir.module1" in mf.modules_loaded
     assert len(mf.modules_loaded) == 1
 
 
 def test_partial_id_2():
     mf = init_mf()
-    mf.run(modules=["parentdir.module1"])
+    mf.run(argv=[], modules=["parentdir.module1"])
     assert "modules.parentdir.module1" in mf.modules_loaded
     assert len(mf.modules_loaded) == 1
 
 
 def test_full_id():
     mf = init_mf()
-    mf.run(modules=["modules.parentdir.module1"])
+    mf.run(argv=[], modules=["modules.parentdir.module1"])
     assert "modules.parentdir.module1" in mf.modules_loaded
     assert len(mf.modules_loaded) == 1
 
@@ -38,7 +38,7 @@ def test_full_id():
 def test_shortid_error():
     mf = init_mf()
     with pytest.raises(ValueError) as excinfo:
-        mf.run(modules=["module2"])
+        mf.run(argv=[], modules=["module2"])
         assert "is not unique" in str(excinfo.value)
     assert len(mf.modules_loaded) == 0
 
@@ -46,34 +46,34 @@ def test_shortid_error():
 def test_partial_id_error():
     mf = init_mf()
     with pytest.raises(ValueError) as excinfo:
-        mf.run(modules=["modules.module2"])
+        mf.run(argv=[], modules=["modules.module2"])
         assert "is not unique" in str(excinfo.value)
     assert len(mf.modules_loaded) == 0
 
 
 def test_partial_id_3():
     mf = init_mf()
-    mf.run(modules=["modules.otherdir.module2"])
+    mf.run(argv=[], modules=["modules.otherdir.module2"])
     assert "modules.otherdir.module2" in mf.modules_loaded
     assert len(mf.modules_loaded) == 1
 
 
 def test_partial_id_4():
     mf = init_mf()
-    mf.run(modules=["modules.otherdir.module2"])
+    mf.run(argv=[], modules=["modules.otherdir.module2"])
     assert "modules.otherdir.module2" in mf.modules_loaded
     assert len(mf.modules_loaded) == 1
 
 
 def test_partial_id_5():
     mf = init_mf()
-    mf.run(modules=["parentdir.module2"])
+    mf.run(argv=[], modules=["parentdir.module2"])
     assert "modules.parentdir.module2" in mf.modules_loaded
     assert len(mf.modules_loaded) == 1
 
 
 def test_partial_id_6():
     mf = init_mf()
-    mf.run(modules=["parentdir.module2"])
+    mf.run(argv=[], modules=["parentdir.module2"])
     assert "modules.parentdir.module2" in mf.modules_loaded
     assert len(mf.modules_loaded) == 1
--- tests/test_import/test_relative_import.py
+++ tests/test_import/test_relative_import.py
@@ -8,7 +8,7 @@ def init_mf():
 
 def test_passive_submodule_import():
     mf = init_mf()
-    mf.run(modules=["module3"])
+    mf.run(argv=[], modules=["module3"])
     assert "modules.parentdir.module3" in mf.modules_loaded
     assert "modules.parentdir.module3.submodule" in mf.modules_loaded
     assert len(mf.modules_loaded) == 2
@@ -16,14 +16,14 @@ def test_passive_submodule_import():
 
 def test_active_submodule_import():
     mf = init_mf()
-    mf.run(modules=["submodule"])
+    mf.run(argv=[], modules=["submodule"])
     assert "modules.parentdir.module3.submodule" in mf.modules_loaded
     assert len(mf.modules_loaded) == 1
 
 
 def test_relative_import():
     mf = init_mf()
-    mf.run(modules=["module4"])
+    mf.run(argv=[], modules=["module4"])
     assert "modules.parentdir.module4" in mf.modules_loaded
     assert "modules.parentdir.module3" in mf.modules_loaded
     assert "modules.parentdir.module3.submodule" in mf.modules_loaded
@@ -32,7 +32,7 @@ def test_relative_import():
 
 def test_relative_import_2():
     mf = init_mf()
-    mf.run(modules=["module5"])
+    mf.run(argv=[], modules=["module5"])
     assert "modules.parentdir.module5" in mf.modules_loaded
     assert "modules.otherdir.module2" in mf.modules_loaded
     assert len(mf.modules_loaded) == 2
