51ee17992d23a5add4ed42e9a8cd13bf1e75fb07
David Hartmann <dhartmann@uni-mainz.de>
2020-12-30 16:44:31 +0100

    tests: added tests for register_event-in-event

---
+++ tests/test_event/modules/register_event_during_event/__init__.py
@@ -0,0 +1,21 @@
+
+def test(i):
+    print("test", i)
+
+
+def add_test(mf):
+    def add_test_print(i):
+        print("added to test", i)
+    mf.register_event("test", add_test_print)
+
+
+def main(event):
+    event.test(0)
+    event.add_test()
+    event.test(1)
+
+
+def register(mf):
+    mf.register_event("test", test)
+    mf.register_event("add_test", add_test)
+    mf.register_event("main", main)
