33c8637a96c6cb86b9b084e67b5ff83270f72d74
David Hartmann <dhartmann@uni-mainz.de>
2021-01-21 18:20:45 +0100

    tests: added outervar test from #61

---
+++ tests/outervar/modules/module2/__init__.py
@@ -0,0 +1,17 @@
+from miniflask import outervar
+
+
+def before_main(*args, var_a=outervar, **kwargs):
+    print("before_main")
+    print("outervar:", var_a)
+    return args, kwargs
+
+
+def main(event):
+    del event
+    print("main")
+
+
+def register(mf):
+    mf.register_event('before_main', before_main, unique=False)
+    mf.register_event('main', main, unique=True)
