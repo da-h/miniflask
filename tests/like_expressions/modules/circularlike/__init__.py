1b7e34179d922f90127a47f4458f1b9c72e4d2d8
David Hartmann <dhartmann@uni-mainz.de>
2021-07-21 17:16:31 +0200

    tests: added test against circular variable dependencies

---
+++ tests/like_expressions/modules/circularlike/__init__.py
@@ -0,0 +1,12 @@
+from miniflask import like
+
+
+def main():
+    print("main event")
+
+
+def register(mf):
+    mf.register_defaults({
+        "foobar": like("foobar", 0)
+    })
+    mf.register_event('main', main)
