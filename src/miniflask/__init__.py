baab2e6b291187a73f034201948cbcbcc541ca19
David Hartmann <dhartmann@uni-mainz.de>
2022-07-21 16:31:51 +0200

    publish minor release 4.3.0

--- src/miniflask/__init__.py
+++ src/miniflask/__init__.py
@@ -4,7 +4,7 @@ from .modules import *
 from .state import like, optional
 
 # meta
-__version__       = "4.2.0"
+__version__       = "4.3.0"
 __title__         = "miniflask"
 __description__   = "Small research-oriented hook-based plugin engine."
 __url__           = "https://github.com/da-h/miniflask"
