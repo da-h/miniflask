7c4a2e081d56f4347d145f1154226d6dd2445d4c
David Hartmann <dhartmann@uni-mainz.de>
2022-10-18 14:35:31 +0200

    tests: added tests for lambda expressions with alternative dependencies
    Split from "fixup! API-change: refactor of state-dependency handling"

--- tests/state/dependencies/modules/dependencychain_module3/__init__.py
+++ tests/state/dependencies/modules/dependencychain_module3/__init__.py
@@ -6,6 +6,5 @@ def register(mf):
         "foo2": lambda: 200,
         "foo3": lambda: 300,
         "foo4": lambda: 400,
-        "foo5": lambda state: 12 + state["..dependencychain_module4.foo5"],
-        # "foo5": lambda state: state["..dependencychain_module4.foo5"] if "..dependencychain_module4.foo5" in state else state["..dependencychain_module1.foo5"],
+        "foo5": lambda state: 12 * state["..dependencychain_module4.foo5"] if "..dependencychain_module4.foo5" in state else (state["..dependencychain_module1.foo3"] // 100),
     })
