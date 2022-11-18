78352046e84ca53a45b8839901fd6946632ecc4d
Sebastian Brodehl <brodehl@uni-mainz.de>
2022-11-18 13:14:24 +0100

    Add more test cases.

--- tests/state/dependencies/modules/lambdaarguments_module1/__init__.py
+++ tests/state/dependencies/modules/lambdaarguments_module1/__init__.py
@@ -4,16 +4,36 @@ def getvalue(state):
     return state["42"]
 
 
-def somefunction(val):
+def some_function(val):
     return val * 300 + 2
+
+
+class TestClass:
+
+    def __init__(self, state):
+        self.state = state
+
+
+lambda_definition = lambda: 42  # noqa
+
+
+lambda_tuple_definition = lambda: 42, lambda: 43  # noqa
 
 
 def register(mf):
     mf.register_defaults({
+        "test_multiple_inline_I": lambda: 1 * 42, "test_multiple_inline_II": lambda: 2 * 42,
+        "test_lambda_def": lambda_definition,
+        "test_class": TestClass,
+        "test_lambda_tuple": lambda_tuple_definition[1],
+        "test_line_breaks": lambda state: state[
+            "var1"
+        ],
+        "test_not_cmp": lambda state: some_function(state["var1"] + 1) * 5 if "var3" not in state and "var3" not in state else state["var3"],
         "var1": lambda: 42,
         "var2": lambda state: state["var1"],
         "var3": lambda state: state['var2'] + state['var1'],
-        "var4": lambda state: somefunction(state["var1"] + 1) * 5,
-        "var5": lambda state: somefunction(state["var1"] + 1) * 5 if "var1" in state else state["var3"],
-        "var6": lambda state: somefunction(state['var1'] + state["var2"]) * 5 if 'var1' in state and "var2" in state else state['var3'] + state["var4"],
+        "var4": lambda state: some_function(state["var1"] + 1) * 5,
+        "var5": lambda state: some_function(state["var1"] + 1) * 5 if "var1" in state else state["var3"],
+        "var6": lambda state: some_function(state['var1'] + state["var2"]) * 5 if 'var1' in state and "var2" in state else state['var3'] + state["var4"],
     })
